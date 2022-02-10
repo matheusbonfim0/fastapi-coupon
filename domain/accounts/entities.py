ca35dfc5268d88d145bc681ff6ff4fd74af4466c
Matheus Bonfim <math.bonfim1@gmail.com>
2022-02-10 17:03:57 -0400

    Refartoring

---
+++ domain/accounts/entities.py
@@ -0,0 +1,14 @@
+from enum import Enum
+from typing import List
+from pydantic import UUID4, BaseModel, EmailStr
+
+class UserRole(str, Enum):
+    admin = ''
+    tourist = 'tourist'
+
+class User(BaseModel):
+    id: UUID4
+    name: str
+    email: EmailStr
+    company_id: UUID4
+    roles: List[UserRole]
